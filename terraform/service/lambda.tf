10c2c222ab7e4915aa094ae32b41c3d840d0453a
Yoyu <yoyu.li@cloudreach.com>
2020-03-28 09:36:05 +0000

    adding model selection function

--- terraform/service/lambda.tf
+++ terraform/service/lambda.tf
@@ -49,6 +49,27 @@ resource "aws_lambda_function" "input_file_checker" {
   }
 }
 
+resource "aws_lambda_function" "model_selection" {
+  filename      = "../../lambda_functions/archive.zip"
+  source_code_hash = data.archive_file.lambda_functions.output_base64sha256
+  function_name = "${var.PROJECT_NAME}-${var.ENVIRONMENT}-Model-Selection"
+  role          = module.lambda_model_selection_role.arn
+  handler       = "model_selection.run"
+  runtime       = "python3.8"
+
+  environment {
+    variables = {
+      S3_BUCKET_NAME = var.S3_BUCKET_NAME
+      REGION = var.REGION
+    }
+  }
+
+  tags = {
+    Project     = var.PROJECT_NAME
+    Environment = var.ENVIRONMENT
+  }
+}
+
 # s3_bucket_notification for object watcher
 
 resource "aws_lambda_permission" "allow_bucket" {
@@ -132,4 +153,18 @@ module "lambda_input_file_checker_role" {
   services     = ["lambda.amazonaws.com"]
   policy_arns  = [aws_iam_policy.s3_data_bucket_role_policy.arn,
                   aws_iam_policy.cloudwatch_logger_role_policy.arn]
+}
+
+
+// Lambda-Model-Selection-Role
+
+
+module "lambda_model_selection_role" {
+  source = "../modules/iam_role"
+
+  PROJECT_NAME = var.PROJECT_NAME
+  ENVIRONMENT  = var.ENVIRONMENT
+  name         = "Lambda-Model-Selection-Role"
+  services     = ["lambda.amazonaws.com"]
+  policy_arns  = [aws_iam_policy.state_machine_role_policy.arn]
 }
